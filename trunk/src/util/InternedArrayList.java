3bac7fd1133416182a3d921635276ec8ef141f96
ashwin.nr@gmail.com <ashwin.nr@gmail.com@9b319328-f1a0-087f-ee39-4de961afe6ce>
2014-04-26 06:00:36 +0000

    interned arraylist for ordering; using == for indexOf

---
+++ trunk/src/util/InternedArrayList.java
@@ -0,0 +1,27 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class InternedArrayList<T> extends ArrayList<T> {
+    	public InternedArrayList(ArrayList<T> ordering) {
+    	    super();
+    	    addAll(ordering);
+    	}
+    	
+    	@Override
+    	public int indexOf(Object o) {
+    	    Iterator<T> it = super.iterator();
+    	    int idx = 0;
+    	    
+    	    while( it.hasNext() ){
+    		if( ((T)o) == it.next() ){
+    		    return idx;
+    		}
+    		++idx;
+    	    }
+    	    
+    	    return -1;
+    	    
+    	}
+}
